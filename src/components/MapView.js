84931f8ea6ece7ad668a5d7d0a736654af79553f
Daniel <mwdchang@gmail.com>
2016-11-26 06:42:07 -0500

    use smoother (bezier)

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -29,9 +29,24 @@ export default class MapView extends Component {
 
 
     data.forEach((d)=> {
-      window.L.hotline([[+d.f_lat, +d.f_lng, 10], [+d.t_lat, +d.t_lng, 20]], {
+      let x1 = +d.f_lat;
+      let y1 = +d.f_lng;
+      let x2 = +d.t_lat;
+      let y2 = +d.t_lng;
+
+      let midx = x1 + (x2-x1)/2;
+
+      let bezier = new window.Bezier(x1, y1, midx, y1, midx, y2, x2, y2)
+      let points = bezier.getLUT(20);
+      let pointsPrime = points.map((p, i) => {
+        return [p.x, p.y, i];
+      });
+
+
+      window.L.hotline(pointsPrime, {
+      //window.L.hotline([[+d.f_lat, +d.f_lng, 10], [+d.t_lat, +d.t_lng, 20]], {
       //window.L.hotline([[0, 0, 10], [40, 40, 15], [70, 70, 20]], {
-        min: 10,
+        min: 0,
         max: 20,
         palette: {
           0.0: '#00CC00',
@@ -50,7 +65,7 @@ export default class MapView extends Component {
   render() {
     return (
       <div style={{ marginTop: `50px` }}>
-        <div id="mapview" style={{ height: `300px`, width: `100vw` }} />
+        <div id="mapview" style={{ height: `350px`, width: `100vw` }} />
       </div>
     )
   }
