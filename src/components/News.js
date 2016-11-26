c9296fe24163fc44303ac335edab53fb3687f65c
Daniel <mwdchang@gmail.com>
2016-11-26 13:42:50 -0500

    news component

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import _ from 'lodash'
+import React, { Component } from 'react'
+import { style as css } from 'glamor'
+import * as d3 from 'd3'
+
+export default class News extends Component {
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div style={{margin: '5px'}}>
+         <div style={{height: '300px', width: '550px'}}>TODO</div>
+      </div>
+    )
+  }
+}
